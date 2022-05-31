c4831abd442fc490e36eb02097ab0e3b50add88c
lucas2251 <lucascordoba2@hotmail.com>
2022-05-31 20:33:49 -0300

    Añadiendo archivos de la clase 15, Destructuring y Objeto date

---
+++ app.js
@@ -0,0 +1,10 @@
+let luz = "rojo"
+switch (luz) {
+    case "rojo":
+        console.log("la persona no puede pasar");
+        break;
+
+    default:
+        console.log("no hay otra loz");
+        break;
+}
